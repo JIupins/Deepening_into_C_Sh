ea35bbc08e8aff86c5ba496d1ee3e6878c821f26
AlekseyBaboshin <BaboshinAP@gmail.com>
2024-08-27 15:38:25 +0300

    The program S2_HW5 is created

---
+++ S2_HW5/OperandChangedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2_HW5
+{
+    public class OperandChangedEventArgs : EventArgs
+    {
+        public OperandChangedEventArgs(double operand)
+        {
+            Operand = operand;
+        }
+
+        public double Operand { get; }
+    }
+}
---
+++ S2_HW5/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace S2_HW5
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            string action = "";
+
+            ICalculator calculator = new Calculator();
+            calculator.GotResult += Calculator_GotResult;
+
+            while (true)
+            {
+                Console.Write($"Введите {counter}-е число: ");
+                string input = Console.ReadLine();
+                if (input.Equals("q", StringComparison.OrdinalIgnoreCase) || string.IsNullOrWhiteSpace(input)) return;
+
+                double number = ReadDouble(input);
+
+                Console.Write("Введите арифметическое действие (+, -, *, /) или 'q' для выхода: ");
+                action = Console.ReadLine();
+                switch (action)
+                {
+                    case "+":
+                        calculator.Sum(number);
+                        break;
+                    case "-":
+                        calculator.Substract(number);
+                        break;
+                    case "*":
+                        calculator.Multiply(number);
+                        break;
+                    case "/":
+                        calculator.Divide(number);
+                        break;
+                    case "q":
+                        return;
+                    default:
+                        Console.WriteLine("Неверное действие!");
+                        Console.WriteLine("Начните сначала.");
+                        Console.WriteLine();
+                        counter--;
+                        break;
+                }
+                counter++;
+            }
+        }
+
+        private static void Calculator_GotResult(object sender, OperandChangedEventArgs e)
+        {
+            Console.WriteLine($"Ответ: {e.Operand}");
+            Console.WriteLine();
+        }
+
+        private static double ReadDouble(string input)
+        {
+            double result;
+            while (!double.TryParse(input, out result))
+            {
+                Console.WriteLine("Это не число!");
+                Console.WriteLine("Введите число.");
+                input = Console.ReadLine();
+            }
+            return result;
+        }
+    }
+}
